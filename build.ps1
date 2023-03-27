f441d41d87b457965ef6e3d16eed0014a3e579c1
Rob Hughes <xman256@gmail.com>
2023-03-27 12:01:30 +0100

    Rename Viewer -> Tracker

--- build.ps1
+++ build.ps1
@@ -6,12 +6,12 @@ cargo build --target wasm32-unknown-unknown
 # This creates a javascript file and also a modified version of the wasm
 # (removing some metadata that was added during the rust build)
 Write-Output "wasm-bindgen..."
-wasm-bindgen --target no-modules --out-dir target\wasm32-unknown-unknown\debug\wasm-bindgen target\wasm32-unknown-unknown\debug\tournament-viewer.wasm
+wasm-bindgen --target no-modules --out-dir target\wasm32-unknown-unknown\debug\wasm-bindgen target\wasm32-unknown-unknown\debug\tournament-tracker.wasm
 
 # Turn the modifed wasm binary into a javascript hardcoded array of bytes. This means we can load it
 # using a file:// origin, without having to set up a web server (we can't load the binary file due to CORS)
 Write-Output "JS hardcoded array..."
-$wasmBytes = [System.IO.File]::ReadAllBytes("target\wasm32-unknown-unknown\debug\wasm-bindgen\tournament-viewer_bg.wasm")
+$wasmBytes = [System.IO.File]::ReadAllBytes("target\wasm32-unknown-unknown\debug\wasm-bindgen\tournament-tracker_bg.wasm")
 $js = [System.Text.StringBuilder]::new();
 [void]$js.Append("var WASM_BYTES = new Uint8Array([");
 foreach ($b in $wasmBytes) {
@@ -19,4 +19,4 @@ foreach ($b in $wasmBytes) {
     [void]$js.Append(",");
 }
 [void]$js.AppendLine("]);");
-[System.IO.File]::WriteAllText("target\wasm32-unknown-unknown\debug\wasm-bindgen\tournament-viewer_bg.wasm.js", $js);
+[System.IO.File]::WriteAllText("target\wasm32-unknown-unknown\debug\wasm-bindgen\tournament-tracker_bg.wasm.js", $js);
